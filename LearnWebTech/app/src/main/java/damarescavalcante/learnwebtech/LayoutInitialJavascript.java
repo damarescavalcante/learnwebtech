003566f6b6c5aaebd45e2919d8a9465a06e94626
Damares Cavalcante <damaresscavalcante@gmail.com>
2016-03-17 08:32:13 -0300

    Criação do layout inicial

---
+++ LearnWebTech/app/src/main/java/damarescavalcante/learnwebtech/LayoutInitialJavascript.java
@@ -0,0 +1,16 @@
+package damarescavalcante.learnwebtech;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by damarescavalcante on 16/03/16.
+ */
+public class LayoutInitialJavascript extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedStateInstance){
+        super.onCreate(savedStateInstance);
+        setContentView(R.layout.layout_initial_javascript);
+    }
+
+}
